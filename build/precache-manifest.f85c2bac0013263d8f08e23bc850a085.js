e90b9029ab52460d4597f3fa756e70591c2756a1
Ivan Rodriguez <sbr.com@hotmail.com>
2020-10-04 17:50:28 -0500

    adding build

---
+++ build/precache-manifest.f85c2bac0013263d8f08e23bc850a085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f31cf443dc66cb89f17b4b72ff8db5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1259427386ecb2b642bc",
+    "url": "/static/js/2.dffd4f3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dffd4f3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1aa2ccebda9d292abe",
+    "url": "/static/js/main.2cca6fdc.chunk.js"
+  },
+  {
+    "revision": "348c89fc34ea7372aa13",
+    "url": "/static/js/runtime-main.933063f3.js"
+  }
+]);
